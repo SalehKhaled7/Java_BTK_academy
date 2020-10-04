559c8e66e8d34ce7bd31932df692e4e171ea5bba
salehkhaled7 <lost1994soul@gmail.com>
2020-10-04 22:37:17 +0300

    data types

---
+++ datatypes/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //Integer
+            int a = 4;// int- From +2,147,483,647 to -2,147,483,648
+            a = 5;
+
+            byte b = 120;// byte- From +127 to -128
+            b = 2;
+
+            char d='d'; //char
+
+            //long
+            //short
+
+        //Floating-point
+            double c = 2.2 ; //double
+
+            //float
+
+        //String
+            String name = "saleh khaled";
+
+        //boolean
+            boolean state = true; // true ,false
+    }
+}
