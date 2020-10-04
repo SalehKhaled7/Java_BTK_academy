75ead6ae5b88d214d5bf299e316eac86aac3d30f
salehkhaled7 <lost1994soul@gmail.com>
2020-10-04 22:11:13 +0300

    variables and why use them

---
+++ variables/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int bookNumber = 5;// re-usability
+        String msg = "book number :";
+        System.out.println("book number :" + bookNumber);
+        System.out.println("book number :" + bookNumber);
+        System.out.println(msg + bookNumber);
+        System.out.println(msg + bookNumber);
+    }
+}
