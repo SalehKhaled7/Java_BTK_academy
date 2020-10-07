fbd927db1c07e4849ad8d65e7e8fa41575fad6c7
salehkhaled7 <lost1994soul@gmail.com>
2020-10-07 16:13:37 +0300

    if conditions

---
+++ Conditions/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 10;
+        if (a > 20) {
+            System.out.println("a is greater than 20");
+        } else if (a < 20) {
+            System.out.println("a is less than 20");
+        } else {
+            System.out.println("a is equal to 20");
+        }
+        if(a<10){
+            System.out.println("a is less than 10");
+        }else if (a == 10){
+            System.out.println("a is equal to 10");
+        }
+    }
+}
