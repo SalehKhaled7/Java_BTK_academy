4b9122ece5638394020b711181bc3eb6901bfc0c
salehkhaled7 <lost1994soul@gmail.com>
2020-10-11 19:52:40 +0300

    mini project - perfect numbers

---
+++ perfectNumbers/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        //perfect number ex:6 = 1+2+3
+        int number = -5;
+        int sum = 0;
+        // for (int number = 1; number < 100; number++) {
+        //    int sum = 0;
+        if(number==1){
+            System.out.println(number+" is not a perfect number");
+            return;
+        }
+        if(number<0){
+            System.out.println("invalid number");
+            return;
+        }
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        if (sum == number) {
+            System.out.println(number + " is  a perfect number");
+        } else {
+             System.out.println(number + " is not a perfect number");
+        }
+    }
+//}
+}
