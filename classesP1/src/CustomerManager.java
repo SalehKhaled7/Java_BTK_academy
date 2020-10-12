5ffe575a08c38c7be56edd30ba7513283c62aa71
salehkhaled7 <lost1994soul@gmail.com>
2020-10-12 21:02:01 +0300

    Classes:intro

---
+++ classesP1/src/CustomerManager.java
@@ -0,0 +1,16 @@
+public class CustomerManager {
+    public void addCustomer() {
+        //customer adding code
+        System.out.println("customer is added successfully ...");
+    }
+
+    public void removeCustomer() {
+        //customer adding code
+        System.out.println("customer is added successfully ...");
+    }
+
+    public void updateCustomer() {
+        //customer adding code
+        System.out.println("customer is added successfully ...");
+    }
+}
---
+++ classesP1/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+    //a class contain all methods
+    //a class contain all methods that are related to the same topic exp: clientManager
+	// from project src > new > Java class
+
+    }
+}
