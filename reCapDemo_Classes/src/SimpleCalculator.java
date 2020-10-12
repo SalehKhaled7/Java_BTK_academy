93d4ab3b13b8c2714454d1cae818e849630ef74b
salehkhaled7 <lost1994soul@gmail.com>
2020-10-12 23:15:07 +0300

    classes:simple calculator 0.2

---
+++ reCapDemo_Classes/src/SimpleCalculator.java
@@ -0,0 +1,14 @@
+public class SimpleCalculator {
+    public int add(int number1,int number2){
+        return number1+number2;
+    }
+    public int multiplication(int number1,int number2){
+        return number1*number2;
+    }
+    public int subtraction(int number1,int number2){
+        return number1-number2;
+    }
+    public int division(int number1,int number2){
+        return number1/number2;
+    }
+}
