068c6daf3c812455f2c1ac83109b36826ced402e
salehkhaled7 <lost1994soul@gmail.com>
2020-10-12 23:42:37 +0300

    Classes:products example

---
+++ classesWithAttributes/src/ProductManager.java
@@ -0,0 +1,12 @@
+public class ProductManager { // every Class should have only one task ..products managing class
+
+    public void add(Products product) {
+        //code for adding the product to the data base
+        System.out.println("Product added successfully: " + product.name);
+    }
+
+    public void update(Products product) {
+        //code for adding the product to the data base
+        System.out.println("Product updated successfully: " + product.name);
+    }
+}
---
+++ classesWithAttributes/src/Products.java
@@ -0,0 +1,9 @@
+public class Products { // every Class should have only one task
+
+    //object attributes / fields
+    int id;
+    String name;
+    String description;
+    double price;
+    int stockAmount;
+}
