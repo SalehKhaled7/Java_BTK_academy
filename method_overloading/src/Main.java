5e1c6c9e152b848d2def6be82ecaf4a51d494540
salehkhaled7 <lost1994soul@gmail.com>
2020-10-13 22:36:01 +0300

    method overloading

---
+++ method_overloading/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        SimpleCalculator simpleCalculator = new SimpleCalculator();
+        System.out.println(simpleCalculator.add(1,2));
+        System.out.println(simpleCalculator.add(1,2,3));
+    }
+}
---
+++ method_overloading/src/SimpleCalculator.java
@@ -0,0 +1,9 @@
+public class SimpleCalculator {
+
+    public int add(int number1, int number2) {
+        return number1 + number2;
+    }
+    public int add(int number1, int number2,int number3) { // method overloading
+        return number1 + number2+number3;
+    }
+}
