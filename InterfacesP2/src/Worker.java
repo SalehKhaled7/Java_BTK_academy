84bb3a8840fd862a51416ad6816e28d2487086d7
salehkhaled7 <lost1994soul@gmail.com>
2020-10-19 00:35:30 +0300

    Interfaces : bank worker exp

---
+++ InterfacesP2/src/Worker.java
@@ -0,0 +1,19 @@
+//work for us
+public class Worker implements IWorkable, IEatable, IPayable {
+
+    @Override
+    public void work() {
+
+    }
+
+    @Override
+    public void pay() {
+
+    }
+
+    @Override
+    public void eat() {
+
+
+    }
+}
