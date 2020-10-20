e986550a461eb7b42be277cf78fe87f1c787ad12
salehkhaled7 <lost1994soul@gmail.com>
2020-10-20 15:56:46 +0300

    Packages:build-in packages

---
+++ packagesDemo/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;//build-in packages
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter a string: ");
+        String str = scanner.nextLine();
+        System.out.println("the out put is : "+str);
+
+    }
+}
