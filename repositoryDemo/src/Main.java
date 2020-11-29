dcdacd0843b23e5399375373e8d421ae3c590752
SalehKhaled7 <lost1994soul@gmail.com>
2020-11-30 01:38:58 +0300

    Generics (methods)

--- repositoryDemo/src/Main.java
+++ repositoryDemo/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        Validator validator = new Validator();
+        validator.validate(new  Customer());
+        validator.validate(new Product());
+        //validator.validate(new ProductDal()); // new ProductDal fives error because of constraining
     }
 }
--- repositoryDemo/src/Validator.java
+++ repositoryDemo/src/Validator.java
@@ -1,2 +1,7 @@
 public class Validator {
+
+
+    public <T extends IEntity> void validate(T entity){ // generic method
+        //the idea is to use the same method for different class by passing the class type as argument
+    }
 }
